3369a443712f9560308a0437022a958fcb28b90a
Eric VONARX <eric.vonarx@free.fr>
2019-10-07 16:32:30 +0200

    Ajoutez des fichiers projet.

---
+++ Battle/Form1.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Dragon = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.Dragon, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Dragon
+        '
+        Me.Dragon.Image = Global.WindowsApplication1.My.Resources.Resources.colère1
+        Me.Dragon.Location = New System.Drawing.Point(133, 177)
+        Me.Dragon.Name = "Dragon"
+        Me.Dragon.Size = New System.Drawing.Size(32, 32)
+        Me.Dragon.TabIndex = 0
+        Me.Dragon.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(30, 29)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(129, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Multiplicate the object"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(30, 63)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(108, 17)
+        Me.CheckBox2.TabIndex = 2
+        Me.CheckBox2.Text = "Freeze the object"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(31, 99)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(107, 17)
+        Me.CheckBox3.TabIndex = 3
+        Me.CheckBox3.Text = "Delete the object"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(340, 12)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(225, 258)
+        Me.TextBox1.TabIndex = 4
+        '
+        'MainForm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(584, 461)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Dragon)
+        Me.Name = "MainForm1"
+        Me.Text = "Battle"
+        CType(Me.Dragon, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Dragon As System.Windows.Forms.PictureBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Battle/Form1.vb
@@ -0,0 +1,124 @@
+﻿Public Class MainForm1
+
+    'Dim typ As Type
+
+    Private Sub MainForm1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'the window accepts "drag and drop"
+        Me.AllowDrop = True
+    End Sub
+
+    Private Sub MainForm1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles MyBase.DragEnter
+        Me.TextBox1.Text = Me.TextBox1.Text & "Form1_DragEnter" & vbCrLf
+
+        If CheckBox1.Checked = True Then
+            e.Effect = DragDropEffects.Copy
+        Else
+            Me.TextBox1.Text = Me.TextBox1.Text & "DragEnter : C'est ce controle qui a déclenché l'action  " & Me.Controls.IndexOf(sender) & vbCrLf
+            e.Effect = DragDropEffects.Move
+
+            Me.TextBox1.Text = Me.TextBox1.Text & "Au départ e.X = " & e.X & vbCrLf
+        End If
+    End Sub
+
+    Private Sub MainForm1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles MyBase.DragDrop
+
+        Me.TextBox1.Text = Me.TextBox1.Text & "Form1_DragDrop" & vbCrLf
+
+        If CheckBox1.Checked = True Then
+            'multiplication des dragons
+            Dim newDragon As New PictureBox
+            newDragon.Width = 32
+            newDragon.Height = 32
+            newDragon.Image = e.Data.GetData(DataFormats.Bitmap)
+            Controls.Add(newDragon)
+            newDragon.Left = e.X - (Me.Left + 25)
+            newDragon.Top = e.Y - (Me.Top + 40)
+            Me.TextBox1.Text = Me.TextBox1.Text & "Multiplication de dragons" & vbCrLf
+            'Ici on ajoute un évenement MouseDown et lorsqu'il survient on appelle Dragon_MouseDown
+            AddHandler newDragon.MouseDown, AddressOf Dragon_MouseDown
+            AddHandler newDragon.MouseHover, AddressOf Dragon_MouseHover
+            Me.TextBox1.Text = Me.TextBox1.Text & "New Dragon TabIndex = " & newDragon.TabIndex & vbCrLf
+
+
+        Else
+            'déplacement de dragons
+            'la nouvelle position du dragon est la position du curseur sur la fenêtre - la position du dragon à l'origine - la position du curseur sur la dragon à l'origine
+            'Me.TextBox1.Text = Me.TextBox1.Text & "Déplacement de dragons" & vbCrLf
+            Dragon.Left = e.X - (Me.Left + 25)
+            Dragon.Top = e.Y - (Me.Top + 40)
+            Me.TextBox1.Text = Me.TextBox1.Text & "A la fin e.X = " & e.X & vbCrLf
+
+        End If
+
+    End Sub
+
+    Private Sub Dragon_MouseHover(ByVal sender As Object, ByVal e As EventArgs) Handles Dragon.MouseHover
+
+        Me.TextBox1.Text = Me.TextBox1.Text & "MouseHover " & vbCrLf
+        Me.TextBox1.Text = Me.TextBox1.Text & "C'est ce controle qui a déclenché l'action  " & Me.Controls.IndexOf(sender) & vbCrLf
+
+    End Sub
+
+
+    Private Sub Dragon_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Dragon.MouseDown
+
+        If CheckBox3.Checked = True Then
+            sender.dispose()
+
+            Dim n As Integer = 0
+
+            'ici on regarde combien d'éléments de type PictureBox il nous reste
+            Dim controlPictureBox As Control
+            For Each controlPictureBox In Controls
+                If TypeOf controlPictureBox Is PictureBox Then
+                    n = n + 1
+                End If
+            Next controlPictureBox
+
+            's'il en reste 0 alors le jeu est terminé
+            If n = 0 Then
+                n = MsgBox("All the dragons have been killed", MsgBoxStyle.Critical, "Mission accomplished")
+                'fin du programme
+                End
+            End If
+
+
+        ElseIf CheckBox2.Checked = False Then
+            'Sur l'object Dragon du type PictureBox, il y a un control sur le composant avec représentation visuelle.
+            'Sur l'object control il y a une méthode qui s'appelle DoDragDrop et qui démarre une opération glisser-déplacer.
+            'la méthode prend une donnée de type objet
+            Me.TextBox1.Text = Me.TextBox1.Text & "DoDragDrop " & vbCrLf
+            Dragon.DoDragDrop(Dragon.Image, DragDropEffects.All)
+            Me.TextBox1.Text = Me.TextBox1.Text & "DoDragDrop done " & vbCrLf
+        End If
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox3.Checked = True Then
+            CheckBox1.Checked = False
+        End If
+    End Sub
+
+    Private Sub CheckBox2_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox2.CheckedChanged
+        If CheckBox3.Checked = True Then
+            CheckBox2.Checked = False
+        End If
+
+    End Sub
+
+    Private Sub CheckBox3_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox3.CheckedChanged
+        If CheckBox3.Checked = True Then
+            'Ici on met la croix pour représenter le curseur
+            Me.Cursor = Cursors.Cross
+            CheckBox1.Checked = False
+            CheckBox2.Checked = False
+        Else
+            Me.Cursor = Cursors.Arrow
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+End Class
